16ca2b2f44f3aa2498706b0ffe5481105dc44272
Gianluca Messina <gianluca.messina8201@gmail.com>
2024-07-23 16:47:47 +0200

    metodo per annoBisestile

--- src/esercizio1/Main.java
+++ src/esercizio1/Main.java
@@ -9,7 +9,11 @@ public class Main {
         System.out.println("Scrivi qualcosa");
         String stringaPerVerificareCaratteri = scanner.nextLine();
         System.out.println(stringaPariDispari(stringaPerVerificareCaratteri));
-        
+
+        System.out.println("inserisci un anno per verificare se è bisestile");
+        int year = Integer.parseInt(scanner.nextLine());
+
+        annoBisestile(year);
     }
 
     public static boolean stringaPariDispari(String stringa) {
@@ -18,6 +22,22 @@ public class Main {
             return true;
         } else {
             return false;
+        }
+    }
+
+    public static boolean annoBisestile(int year) {
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                if (year % 400 == 0) {
+                    return true;
+                } else {
+                    return false; //perché l'anno non è divisibile per 400
+                }
+            } else {
+                return true; //perché l'anno non è divisibile per 100 ma per 4
+            }
+        } else {
+            return false; //perché l'anno non è divisibile per 4
         }
     }
 }
